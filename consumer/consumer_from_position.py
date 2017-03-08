e622ade6af88622050ba37b2e81348cde5de4476
bbr <bbr@affini-tech.com>
2017-03-08 16:54:46 +0100

    feat: test pb offset

--- consumer/consumer_from_position.py
+++ consumer/consumer_from_position.py
@@ -90,8 +90,9 @@ def get_tweet(consumer):
             first_offset = consumer.next()
             first_offset = first_offset.offset
             write_offset(first_offset)
+            cfg_offset = cfg.get("OFFSET","last_offset")
             ############
-            for message in consumer:
+            for message in get_consumer_kafkaConsumer_seek(cfg_offset):
                 if message is not None:
                     current_offset = message.offset
                     print current_offset
